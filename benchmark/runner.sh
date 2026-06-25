b1eb3ab9c63ea0199a381b92371a3154190b4406
pop <go98con@mytum.de>
2026-06-25 07:52:15 +0000

    removed unnecessary file

--- benchmark/runner.sh
+++
@@ -1,8 +0,0 @@
-  for i in {1..1};do
-    for dataset in "${datasets[@]}";do
-      for algo in "${algorithms[@]}";do
-        echo "Measuring Multithreaded Times: Iteration $i, Dataset $dataset, Algorithm $algo"
-        ../build/benchmark/measure_multithreaded "$dataset" "$algo" "0" "$i"
-      done
-    done
-  done
